db3034875d89ca9509eb1efbd29920e685b75c4d
Matheus Martins <matheus.mmag@gmail.com>
2018-11-25 12:20:19 -0200

    Exercício 5 - Atualizar Tesouro

--- pirates/urls.py
+++ pirates/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('', views.ListaTesourosView.as_view(), name='lista'),
 	path('inserir', views.SalvarTesouroView.as_view(), name='inserir'),
 	path('deletar/<int:pk>', views.DeletarTesouroView.as_view(), name='deletar'),
+	path('editar/<int:pk>', views.SalvarTesouroView.as_view(), name='editar'),
 ]
--- pirates/views.py
+++ pirates/views.py
@@ -23,16 +23,30 @@ class ListaTesourosView(View):
 		return render(request, template_name, context)
 
 class SalvarTesouroView(View):
-	def get(self, request):
+	def get(self, request, pk=None):
 		template_name = 'salvar_tesouro.html'
+		if pk:
+			tesouro = Tesouro.objects.get(pk=pk)
+			form = TesouroForm(instance=tesouro)
+			action = "/editar/" + str(pk)
+		else:
+			form = TesouroForm(request.GET)
+			action = "/inserir/"
 		context = {
-			'form': TesouroForm(request.GET)
+			'form': form,
+			'action': action
 		}	
 		return render(request, template_name, context)
 
-	def post(self,request):
+	def post(self,request, pk=None):
 		template_name = 'salvar_tesouro.html'
-		form = TesouroForm(request.POST, request.FILES)
+		if pk:
+			tesouro = Tesouro.objects.get(pk=pk)
+			form = TesouroForm(request.POST, request.FILES, instance=tesouro)
+			action = "/editar/" + str(pk)
+		else:
+			form = TesouroForm(request.POST, request.FILES)
+			action = "/inserir/"
 		context = {
 			'form': form,
 		}
